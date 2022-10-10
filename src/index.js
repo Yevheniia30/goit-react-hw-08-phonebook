29d10d4863ded84bf2e02dc981fc2fb1cd16a972
Yevheniia Suska <susskaya86@gmail.com>
2022-10-10 20:02:54 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 // import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
